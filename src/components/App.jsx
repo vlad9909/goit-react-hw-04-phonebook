c20be14bb7b209c5855e9dac3f16a15c08bfb5c9
vlad9909 <gva9909@gmail.com>
2023-04-21 13:24:53 +0300

    local

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -53,6 +53,20 @@ export class App extends React.Component {
       contacts: prevState.contacts.filter(contact => contact.id !== id),
     }));
   };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
   render() {
     const visibleContacts = this.visibleContact();
 
